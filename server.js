2f306c8582f9f8019d0614f8be89af73bf6c8524
jonNtech <31232174+jonNtech@users.noreply.github.com>
2022-07-01 10:16:30 -0700

    finished product

--- server.js
+++ server.js
@@ -1,63 +1,66 @@
 const express = require('express')
 const app = express()
-const { MongoClient, ObjectId } = require('mongodb')
 const cors = require('cors')
+const {MongoClient, ObjectId } = require('mongodb')
+const { response } = require('express')
+const { request } = require('http')
 require('dotenv').config()
-const PORT = 5000
+const PORT = 8000
 
 let db,
     dbConnectionStr = process.env.DB_STRING,
     dbName = 'sample_mflix',
     collection
 
-
 MongoClient.connect(dbConnectionStr)
-    .then(client => { 
-    console.log('Connected to Database')
-    db = client.db(dbName)
-    collection = db.collection('movies')
+    .then(client => {
+        console.log(`Connected to database`)
+        db = client.db(dbName)
+        collection = db.collection('movies')
     })
 
-app.use(express.urlencoded( { extended: true }))
+app.use(express.urlencoded({extended : true}))
 app.use(express.json())
 app.use(cors())
 
-app.get ("/search", async (req, res) => {
+app.get("/search", async (request,response) => {
     try {
         let result = await collection.aggregate([
             {
-                "$Search" : {
+                "$search" : {
                     "autocomplete" : {
-                        "query": `${req.query.query}`,
+                        "query": `${request.query.query}`,
                         "path": "title",
                         "fuzzy": {
-                            "maxEdits": 2,
+                            "maxEdits":2,
                             "prefixLength": 3
                         }
                     }
                 }
-
             }
-    ]).toArray()
-    res.send(result)
+        ]).toArray()
+        //console.log(result)
+        response.send(result)
     } catch (error) {
-        res.status(500).send({message: error.message})
+        response.status(500).send({message: error.message})
+        //console.log(error)
     }
 })
 
-app.get("/get/:id", async (req, res) => {
-    try{
+app.get("/get/:id", async (request, response) => {
+    try {
         let result = await collection.findOne({
-            "_id":ObjectId(req.params.id)
+            "_id" : ObjectId(request.params.id)
         })
-        res.send(result)
-    }catch (error) {
-        res.status(500).send({ message: error.message })
+        response.send(result)
+    } catch (error) {
+        response.status(500).send({message: error.message})
     }
-})
+}
+)
 
-app.listen(process.env.PORT || PORT, ()=> {
-    console.log(`server is running on port ${PORT}`)
+app.listen(process.env.PORT || PORT, () => {
+    console.log(`Server is running.`)
 })
 
     
